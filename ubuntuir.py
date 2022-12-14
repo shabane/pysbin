4b2bd984c90b63a62fb3c45633c7b554b603749d
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-12-14 03:57:34 +0330

    [feat](write module for the paste.ubuntu.ir)

---
+++ ubuntuir.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import requests
+from headers import HEADERS
+
+def post2PasteUbuntuIr(text: str) -> str:
+    DATA = {
+        'text': f'{text}',
+    }
+
+    response = requests.post('https://paste.ubuntu.ir/', headers=HEADERS, data=DATA)
+
+    return response.url
+
+if __name__ == '__main__':
+    print(post2PasteUbuntuIr(
+        """
+        this is the sample text.
+        to use this script you should write another simple scritp which will let you
+        to post texts on paste.ubuntu.ir.
+
+        the script is simple as this:
+
+        ```python3
+        from pastebin import ubutnuir
+        result = ubutnuir.post2PasteUbuntuIr('put your text here')
+        print(result)
+        ```
+        """
+    ))
